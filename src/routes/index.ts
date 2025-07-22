3159da03b5c01bd64e45c8c68d3a364a888da8e4
Ifechi <ifechi.dev@gmail.com>
2025-07-22 23:56:47 +0100

    feat: Add activity router to API routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import { authRouter } from "./auth.routes";
 import { userRouter } from "./user.routes";
 import { interestRouter } from "./interest.routes";
 import { postRouter } from "./post.routes";
+import { activityRouter } from "./activity.routes";
 
 const basePath = "/api/v1";
 
@@ -11,4 +12,5 @@ export default (app: { use: (...args: any[]) => void }): void => {
   app.use(`${basePath}/user`, asyncHandler(userRouter));
   app.use(`${basePath}/interest`, asyncHandler(interestRouter));
   app.use(`${basePath}/post`, asyncHandler(postRouter));
+  app.use(`${basePath}/activity`, asyncHandler(activityRouter));
 };
